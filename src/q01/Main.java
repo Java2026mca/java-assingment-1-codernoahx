c248407e04159f29f95675802d49e990122877ad
Nomaan Ahmad <113279627+codernoahx@users.noreply.github.com>
2026-04-07 23:04:12 +0530

    Implement diamond shape printing in Main.java
    Refactor the main method to print a diamond shape using asterisks.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,18 +1,37 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        
         for (int i = 0; i < n; i++) {
-            int val = 1;
-            for (int j = 0; j <= i; j++) {
-                System.out.print(val);
-                if (j < i) System.out.print(" ");
-                val = val * (i - j) / (j + 1);
+            int spaces = n - 1 - i;
+            int width = 2 * i + 1;
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < spaces; j++) sb.append(' ');
+            if (width == 1) {
+                sb.append('*');
+            } else {
+                sb.append('*');
+                for (int j = 0; j < width - 2; j++) sb.append(' ');
+                sb.append('*');
             }
-            if (i < n - 1) System.out.println();
+            System.out.println(sb.toString());
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            int spaces = n - 1 - i;
+            int width = 2 * i + 1;
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < spaces; j++) sb.append(' ');
+            if (width == 1) {
+                sb.append('*');
+            } else {
+                sb.append('*');
+                for (int j = 0; j < width - 2; j++) sb.append(' ');
+                sb.append('*');
+            }
+            System.out.println(sb.toString());
         }
     }
 }
