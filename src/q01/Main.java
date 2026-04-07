92feb15f7965dc67f1a9dbef1ae9cff8479167f3
Nomaan Ahmad <113279627+codernoahx@users.noreply.github.com>
2026-04-07 23:02:34 +0530

    Refactor Main.java to print Pascal's triangle
    Removed the code for printing a diamond shape and replaced it with code to print Pascal's triangle.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,37 +1,18 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
+
         for (int i = 0; i < n; i++) {
-            int spaces = n - 1 - i;
-            int width = 2 * i + 1;
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < spaces; j++) sb.append(' ');
-            if (width == 1) {
-                sb.append('*');
-            } else {
-                sb.append('*');
-                for (int j = 0; j < width - 2; j++) sb.append(' ');
-                sb.append('*');
+            int val = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(val);
+                if (j < i) System.out.print(" ");
+                val = val * (i - j) / (j + 1);
             }
-            System.out.println(sb.toString());
-        }
-        
-        for (int i = n - 2; i >= 0; i--) {
-            int spaces = n - 1 - i;
-            int width = 2 * i + 1;
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < spaces; j++) sb.append(' ');
-            if (width == 1) {
-                sb.append('*');
-            } else {
-                sb.append('*');
-                for (int j = 0; j < width - 2; j++) sb.append(' ');
-                sb.append('*');
-            }
-            System.out.println(sb.toString());
+            if (i < n - 1) System.out.println();
         }
     }
 }
